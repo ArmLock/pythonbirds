574ef87ccce430c63a3e37580a3b3e1dde31186f
Romao <romaotec@gmail.com>
2022-07-24 16:36:46 -0300

    Criado atributos Complexo filho.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=40):
+    def __init__(self, *filhos, nome=None, idade=40):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Ronaldo')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Romão'
-    print(p.nome)
-    print(p.idade)
+    Romao = Pessoa(nome='Romão')
+    Ronaldo = Pessoa(Romao, nome='Ronaldo')
+    print(Pessoa.cumprimentar(Ronaldo))
+    print(id(Ronaldo))
+    print(Ronaldo.cumprimentar())
+    print(Ronaldo.nome)
+    print(Ronaldo.idade)
+    for filho in  Ronaldo.filhos:
+        print(filho.nome)
+
