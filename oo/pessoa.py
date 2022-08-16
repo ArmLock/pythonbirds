353cabd2623e1312c14d8f89a1e66e5cf28bdb85
Romao <romaotec@gmail.com>
2022-08-16 00:05:53 -0300

    Implementado sobrescrita de metodo e atributo.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,11 +7,25 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 55
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+class Homem(Pessoa) :
+    def cumprimentar(self):
+        return 'Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos = 3
 
 if __name__ == '__main__':
-    Romao = Pessoa(nome='Romão')
+    Romao = Mutante(nome='Romão')
     Ronaldo = Pessoa(Romao, nome='Ronaldo')
     print(Pessoa.cumprimentar(Ronaldo))
     print(id(Ronaldo))
@@ -26,9 +40,17 @@ if __name__ == '__main__':
     del Ronaldo.olhos
     print(Ronaldo.__dict__)
     print(Romao.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(Ronaldo.olhos)
     print(Romao.olhos)
     print(id(Pessoa.olhos), id(Ronaldo.olhos), id(Romao.olhos))
-
+    print(Pessoa.metodo_estatico(), Ronaldo.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), Ronaldo.nome_e_atributos_de_classe())
+    pessoa = Pessoa ('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(Romao, Pessoa))
+    print(isinstance(Romao, Homem))
+    print(Romao.olhos)
+    print(Ronaldo.cumprimentar())
+    print(Romao.cumprimentar())
